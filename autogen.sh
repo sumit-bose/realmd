283dc3bba3a7c010d20407aa6d32f6f9848d4d3b
Krzesimir Nowak <knowak@microsoft.com>
2022-03-16 15:18:22 +0100

    Use autoreconf and gettext
    Simplify autogen script a bit by letting autoreconf to do all the work
    with invoking automake, autoconf, autopoint and other autotools.
    
    Also migrate the i18n infra from intltool to gettext. Intltool is
    obsolete by now - gettext gained support for the missing file formats
    by now.

--- autogen.sh
+++ autogen.sh
@@ -10,8 +10,6 @@ PROJECT=realmd
 TEST_TYPE=-f
 FILE=service/realm-daemon.c
 
-DIE=0
-
 # Some boiler plate to get git setup as expected
 if test -d .git; then
 	if test -f .git/hooks/pre-commit.sample && \
@@ -20,34 +18,13 @@ if test -d .git; then
 	fi
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+(autoreconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have autoconf installed to compile $PROJECT."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
-	DIE=1
-}
-
-AUTOMAKE_VERSIONS="1.16 1.15 1.14 1.13 1.12 1.11 1.10"
-for version in $AUTOMAKE_VERSIONS; do
-	if automake-$version --version < /dev/null > /dev/null 2>&1 ; then
-		AUTOMAKE=automake-$version
-		ACLOCAL=aclocal-$version
-		break
-	fi
-done
-
-if test -z "$AUTOMAKE"; then
-	echo
-	echo "You must have one of automake $AUTOMAKE_VERSIONS to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
-	DIE=1
-fi
-
-if test "$DIE" -eq 1; then
 	exit 1
-fi
+}
 
 test $TEST_TYPE $FILE || {
 	echo "You must run this script in the top-level $PROJECT directory"
@@ -73,14 +50,8 @@ rm -rf autom4te.cache
 # regenerated from their corresponding *.in files by ./configure anyway.
 touch README INSTALL
 
-$ACLOCAL $ACLOCAL_FLAGS || exit $?
+autoreconf --verbose --force --install || exit $?
 
-intltoolize --force --copy || exit $?
-
-autoheader || exit $?
-
-$AUTOMAKE --add-missing || exit $?
-autoconf || exit $?
 cd $ORIGDIR || exit $?
 
 if test -z "$NOCONFIGURE"; then
